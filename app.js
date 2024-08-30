32193da15b932e2f506f424704cf7c2298c1e3ff
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2024-08-30 19:28:31 +0900

    프로젝트 세팅 (app.js , assets)
    # 프로젝트를 초기화합니다.
    yarn init -y
    
    # express와 mongoose를 yarn을 이용해 설치합니다.
    yarn add express mongoose
    
    {
      "name": "sparta_memo_site_1",
      "version": "1.0.0",
      "main": "index.js",
      "repository": "https://github.com/YongHyeon1231/sparta_memo_site_1.git",
      "author": "YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>",
      "license": "MIT",
      "type": "module",
      "dependencies": {
        "express": "^4.19.2",
        "mongoose": "^8.6.0"
      }
    }
    
    app.js
    간단한 HTTP 요청을 받을 수 있도록 express 라이브러리를 이용하여 서버를 만듭니다.
    우리는 Node.js 서버 하나로 API와 프론트엔드 파일을 모두 제공합니다.
    백앤드 부분은 /api를 통한 주소로 제공할거고
    프론트 앤드 부분은 /api가 붙지 않는 부분으로 제공합니다.
    
    assets 파일을 서빙할 수 있도록 static 미들웨어를 추가합니다.
    app.js 수정
    
    최종
    http://localhost:3000/

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Express에서 req.body에 접근하여 body 데이터를 사용할 수 있도록 설정합니다.
+app.use(express.json()); // 미들웨어 등록
+app.use(express.urlencoded({ extended: true }));
+
+// static Middleware, express.static()을 사용하여 정적 파일을 제공합니다.
+// 해당하는 프론트앤드 파일을 서빙합니다.
+// ./assets위치에 있는 모든 파일을 서빙할 겁니다.
+app.use(express.static('./assets'));
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  return res.json({ message: 'Hi!' });
+});
+
+// 해당하는 router를 전역미들웨어로 등록해서 /api가 붙은 경우에만 접근 가능하게 만들었다.
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
